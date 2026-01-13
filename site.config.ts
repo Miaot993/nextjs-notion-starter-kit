e01b023dfe3fcf9f7dbe06d0bb8bf52f89192e37
Miaot <142857046+Miaot993@users.noreply.github.com>
2026-01-13 17:27:37 +0800

    Comment out GitHub username in site.config.ts
    Comment out the GitHub username in site.config.ts.

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   //twitter: 'transitive_bs',
-  github: 'null',
+  //github: 'null',
   //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
